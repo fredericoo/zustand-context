b5de502495d410c849f6d3ba287c1c81d4fcf522
Thanaen <thanaen.dev@pm.me>
2024-04-30 21:58:15 +0200

    refactor: don't use react's default export

--- src/context.tsx
+++ src/context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 import type { StoreApi, UseBoundStore } from "zustand";
 
 export const createZustandContext = <
@@ -13,14 +13,14 @@ export const createZustandContext = <
     children?: React.ReactNode;
     initialValue: TInitial;
   }) => {
-    const [store] = React.useState(() => getStore(props.initialValue));
+    const [store] = useState(() => getStore(props.initialValue));
     return <Context.Provider value={store}>{props.children}</Context.Provider>;
   };
 
   return [
     Provider,
     ((selector: Parameters<TStore>[0]) => {
-      const store = React.useContext(Context);
+      const store = useContext(Context);
       if (store === null) {
         console.error("Missing provider for context:", Context);
         throw new Error("Missing provider for context");
