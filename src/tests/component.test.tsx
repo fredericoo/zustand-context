d095160918b7c51932fafe249880d52d3293cf18
Grigoris Christainas <grigorisxristainas@gmail.com>
2024-05-22 18:54:21 +0300

    refactor: move tests directory in src

--- src/tests/component.test.tsx
+++ src/tests/component.test.tsx
@@ -2,7 +2,7 @@ import { afterEach, expect, test } from 'bun:test';
 import { cleanup, render, screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import { create } from 'zustand';
-import { createZustandContext } from '../src';
+import { createZustandContext } from '../context';
 
 type Cat = string;
 
